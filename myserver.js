8585af1c9ea0179487536d4e5bd219b2ef6072e9
vnci12 <mohayowane@gmail.com>
2026-04-16 11:07:19 +0300

    ajout du serveurs

---
+++ myserver.js
@@ -0,0 +1,20 @@
+/**
+ * jle fichier serveur.js a pour mission de créer un serveur de l'application
+ */
+
+//j'importe le package http
+const http = require('http');
+
+//j'importe le fichier app.js
+const app = require('./app');
+
+//je déclare une variable app qui importe le fichier app.js
+const server = http.createServer(app);
+
+//je déclare une variable port qui contient le port d'écoute du serveur
+const numeroPort = 3000;
+
+//je fais écouter le serveur sur le port 3000
+server.listen(numeroPort, () => {
+    console.log(`Le serveur est à l\'écoute sur le port, numeroPort`);
+});
