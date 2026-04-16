6236e256e853c85ca1b7891f1c9c19f7383886ce
vnci12 <mohayowane@gmail.com>
2026-04-16 11:39:27 +0300

    j'initie la vue accueil et le contrelleur accueil

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+ * Le fichier app.js a pour mission de créer une application express pour gérer les requêtes et les réponses du serveur
+ */
+//j'importe le package express
+const express = require('express');
+
+//je crée une application express
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//j'exporte l'application pour pouvoir l'utiliser dans le fichier serveur.js
+module.exports = app;
---
+++ contrellers/accueil.js
@@ -0,0 +1,8 @@
+/**
+ * Ce fichier et un controller qui a pour mission de gérer les requêtes et les réponses pour la page d'accueil de l'application
+ * Dans ce fichier, je vais créer les routes pour la page 'accueil.ejs'
+ */
+
+const express = require('express');
+
+const router = express.Router();
